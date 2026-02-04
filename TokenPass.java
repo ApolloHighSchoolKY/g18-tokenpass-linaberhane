ac72bf1ff2dd43dd4af3a2e43138ace2b87e0138
linaberhane <lina.berhane@stu.daviess.kyschools.us>
2026-02-04 17:54:35 +0000

    Commit

--- TokenPass.java
+++ TokenPass.java
@@ -1,9 +1,23 @@
 public TokenPass(int playerCount){
   /*  to be answered in part (a) */
+  int[] board = new int[playerCount];
+  currentPlayer = Math.random()*playerCount-1;
+  for(int player: board)
+  {
+    player = Math.random()*10;
+  }
   
 }
 
 public void distributeCurrentPlayerTokens(){
   /* to be answered in part (b) */
-  
+  int tokens = board[currentPlayer];
+  if(tokens > 0)
+  {
+    for(int i = currentPlayer; tokens > 0; i++)
+    {
+      board[i] = 0;
+      board[i+1]++;
+    }
+  }
 }
